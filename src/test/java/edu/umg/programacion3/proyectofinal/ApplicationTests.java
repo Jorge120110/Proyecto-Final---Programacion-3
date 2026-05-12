6bc8cd3862fc08bd0056b10f4b7170d2c66ed9a5
Fabian20230 <djimenezl7@miumg.edu.gt>
2026-05-11 20:56:49 -0600

    Inicializacion de proyecto

---
+++ src/test/java/edu/umg/programacion3/proyectofinal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.umg.programacion3.proyectofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
