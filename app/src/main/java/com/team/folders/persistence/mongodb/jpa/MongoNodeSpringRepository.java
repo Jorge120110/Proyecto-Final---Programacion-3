92b9923c481b052e016a1bcc8b928369e0e3d553
Fabian20230 <djimenezl7@miumg.edu.gt>
2026-05-13 20:07:49 -0600

    Restructure project based on updated architecture and stabilize MongoDB setup

--- app/src/main/java/com/team/folders/persistence/mongodb/jpa/MongoNodeSpringRepository.java
+++ app/src/main/java/com/team/folders/persistence/mongodb/jpa/MongoNodeSpringRepository.java
@@ -4,4 +4,4 @@ import com.team.folders.persistence.mongodb.entity.MongoNodeEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MongoNodeSpringRepository extends MongoRepository<MongoNodeEntity, String> {
-}
+} 
